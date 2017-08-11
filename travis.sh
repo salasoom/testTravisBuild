9db3fa39a1f754ccfab7e6a8bdd94367d3e1d618
Markus Salasoo <salasoo1@llnl.gov>
2017-08-11 15:30:02 -0700

    Add debug commands to determine Travis OSX compiler availability/versions

--- travis.sh
+++ travis.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 set -e
 
+gcc --version
+clang --version
+
 mkdir build
 cd build
 cmake .. -DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98} -DCMAKE_VERBOSE_MAKEFILE=TRUE
