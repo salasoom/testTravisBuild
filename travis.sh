6ba1920ac67f91d128288d647a8a6f7f7085691e
Markus Salasoo <salasoo1@llnl.gov>
2017-08-10 18:00:44 -0700

    Try setting CMAKE_C_STANDARD through build matrix (env var), to build script, as an arg to cmake build command

--- travis.sh
+++ travis.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
 set -e
 
+echo "C_STANDARD IS: "
+echo ${C_STANDARD}
+echo "\n"
+
 mkdir build
 cd build
-cmake ..
+cmake .. -DCMAKE_C_STANDARD=${C_STANDARD}
 cmake --build .
 ctest -V -C "Debug"
 
