a73058fedba47e09dfdc597ac1080b4ea5a6b013
Markus Salasoo <salasoo1@llnl.gov>
2017-08-11 16:38:12 -0700

    Remove debugging output from travis builds

--- travis.sh
+++ travis.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env sh
 set -e
 
-gcc --version
-clang --version
-
 mkdir build
 cd build
-cmake .. -DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98} -DCMAKE_VERBOSE_MAKEFILE=TRUE
+cmake .. -DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98}
 cmake --build .
 ctest -V -C "Debug"
 
