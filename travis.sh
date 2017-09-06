abb4a1b452f8c80bbc18d942d0a88bdd76edfc82
Markus Salasoo <salasoo1@llnl.gov>
2017-09-06 11:46:43 -0700

    Temporarily disable running tests

--- travis.sh
+++ travis.sh
@@ -5,5 +5,5 @@ mkdir build
 cd build
 cmake .. -DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98} -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-Debug}
 cmake --build .
-ctest -V -C "${BUILD_TYPE:-Debug}"
+#ctest -V -C "${BUILD_TYPE:-Debug}"
 
