226e3cf5efe1a554bb47610a187077242cec21a0
Markus Salasoo <salasoo1@llnl.gov>
2017-09-06 11:37:27 -0700

    Attempt coveralls with cpp-coveralls

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@ set -e
 
 mkdir build
 cd build
-cmake .. -DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98}
+cmake .. -DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98} -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-Debug}
 cmake --build .
-ctest -V -C "Debug"
+ctest -V -C "${BUILD_TYPE:-Debug}"
 
